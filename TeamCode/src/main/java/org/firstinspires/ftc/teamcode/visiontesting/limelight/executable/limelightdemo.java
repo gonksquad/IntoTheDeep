fa7cc2bf9198e4e46903a8125051cce37ef6ec0b
Ronan <spacechickenrobot@gmail.com>
2024-12-05 17:32:12 -0500

    walle

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/visiontesting/limelight/executable/limelightdemo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/visiontesting/limelight/executable/limelightdemo.java
@@ -1,25 +1,40 @@
 package org.firstinspires.ftc.teamcode.visiontesting.limelight.executable;
 
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.hardware.limelightvision.LLResultTypes;
 import com.qualcomm.hardware.limelightvision.LLStatus;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.acmerobotics.dashboard.FtcDashboard;
-import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.Hardware;
 
 import java.util.List;
 
 @TeleOp(name = "FTCDASH Limelight")
+@Config
 public class limelightdemo extends LinearOpMode {
 
     private Limelight3A limelight;
     public Servo headlight;
     private FtcDashboard dashboard;
+    public Hardware hardware;
     public String color;
     public String output1;
+    public Double wrist_target;
+    public int error;
+    public static double kP = 0.00001;
+    public static double kI = 0.00001;
+    private ElapsedTime runtime = new ElapsedTime();
+    private double dT = 0.0; // Variable to store the last time
+    private double lastTime = 0;
+    private double currentTime = 0;
+    private double integral = 0;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -27,14 +42,22 @@ public class limelightdemo extends LinearOpMode {
         headlight = hardwareMap.get(Servo.class, "headlight");
         dashboard = FtcDashboard.getInstance();
 
+        hardware = new Hardware(hardwareMap);
+
+        wrist_target = 0.55;
+
         limelight.pipelineSwitch(0);
         limelight.start();
 
         TelemetryPacket packet = new TelemetryPacket();
         packet.put("Status", "Robot Ready. Press Play.");
         dashboard.sendTelemetryPacket(packet);
+        hardware.grabberSpin.setPosition(0.55);
         waitForStart();
         headlight.setPosition(1);
+//        hardware.setReachyReachyPosition(-400, 0.3);
+
+        runtime.reset();
 
         while (opModeIsActive()) {
             packet = new TelemetryPacket(); // Create a new packet for each loop
@@ -47,6 +70,29 @@ public class limelightdemo extends LinearOpMode {
             packet.put("Pipeline Index", status.getPipelineIndex());
             packet.put("Pipeline Type", status.getPipelineType());
 
+            currentTime = runtime.seconds();
+            dT = currentTime - lastTime;
+            lastTime = currentTime;
+            integral = error*dT;
+            packet.put("integral", integral);
+            packet.put("time", currentTime);
+
+            wrist_target += error*kP + integral*kI;
+
+            hardware.grabberSpin.setPosition(wrist_target);
+
+            if (wrist_target < 0) {
+                wrist_target = 0.0;
+            } else if (wrist_target > 0.7) {
+                wrist_target = 0.7;
+            }
+
+            if (error < -10) {
+                error = -10;
+            } else if (error > 10) {
+                error = 10;
+            }
+
             LLResult result = limelight.getLatestResult();
             if (result != null) {
                 packet.put("LL Latency", result.getCaptureLatency() + result.getTargetingLatency());
@@ -54,7 +100,6 @@ public class limelightdemo extends LinearOpMode {
 
                 output1 = java.util.Arrays.toString(result.getPythonOutput());
 
-                // Correct method name to getPythonOutputs()
                 packet.put("Python Output", output1);
 
                 if (output1 == "0.0") {
@@ -69,9 +114,13 @@ public class limelightdemo extends LinearOpMode {
 
                 packet.put("LIMELIGHT SAYS THE BLOCK IS", color);
 
+                error = (240 - (int) (result.getPythonOutput()[2]));
 
+                packet.put("error", error);
 
                 if (result.isValid()) {
+
+
                     packet.put("tx", result.getTx());
                     packet.put("txnc", result.getTxNC());
                     packet.put("ty", result.getTy());
